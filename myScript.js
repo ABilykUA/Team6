398c923a38b111a3e3d6cc0afebdbca3c139fa64
Andrii <ABilyk@uclan.ac.uk>
2019-11-08 21:27:47 +0200

    I've made a landing page, feel free to add something or change anything !

--- myScript.js
+++ myScript.js
@@ -1,2 +1,44 @@
 // JS is done here ↓
 
+
+
+    let Title = document.getElementById("Title");
+    let TeamNameElement = document.getElementById("TeamName");
+
+    fetch   ("https://codecyprus.org/th/api/list")
+        .then(response => response.json()) //Parse JSON text to JavaScript object
+        .then(jsonObject => {
+        console.log(jsonObject);
+
+            let TreasureHunts = jsonObject.treasureHunts;
+
+           let TreasureHuntslist = document.getElementById("HuntOption");
+
+           for (let i = 0 ; i<TreasureHunts.length; i++){
+
+               var newBrandOption = document.createElement("option");
+               TreasureHuntslist.appendChild(newBrandOption);
+               newBrandOption.text = TreasureHunts[i].name;
+
+
+
+           }
+
+
+
+
+
+
+
+
+
+
+
+    });
+
+
+
+
+
+
+
