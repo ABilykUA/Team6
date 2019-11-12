9c1d7c82fe28892d41dca3e008f23adad987b08c
Andrii <ABilyk@uclan.ac.uk>
2019-11-12 19:58:30 +0200

    hello guys can you start working on the project plz !!!!!!!

--- myScript.js
+++ myScript.js
@@ -1,23 +1,28 @@
 // JS is done here ↓
-
+//"https://codecyprus.org/th/api/start?player=" + NameElementvalue +  "&app="+ TeamNamevalue +"&treasure-hunt-id=uuid";
 // This is just an example we have to change this ↓ //
     function Output() {
 
-        let output = document.getElementById("test");
+
         let TeamNameElement = document.getElementById("TeamName");
         let NameElement = document.getElementById("Name");
 
         let TeamNamevalue = TeamNameElement.value;
         let NameElementvalue = NameElement.value;
 
-        output.innerHTML = TeamNamevalue + " " + NameElementvalue;
 
+        let startlink = document.createElement("A");
+        document.getElementById("maindiv").appendChild(startlink);
+        startlink.innerHTML =  "Press here ";
+        startlink.setAttribute("href","https://codecyprus.org/th/api/start?player="+  NameElementvalue   + "&app=" + TeamNamevalue +   "&treasure-hunt-id=uuid");
 
     }
 
 // This is just an example we have to change this ↑ //
 
 
+
+function getinformation(){
 
     fetch   ("https://codecyprus.org/th/api/list")
         .then(response => response.json()) //Parse JSON text to JavaScript object
@@ -54,8 +59,9 @@
 
     });
 
+}
 
-
+getinformation();
 
 
 
