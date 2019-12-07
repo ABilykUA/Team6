c79145cde229735101b6116fa513d30130d6777f
Vaximelirus <vivanov1@uclan.ac.uk>
2019-12-07 20:03:55 +0200

    Hide and Seek with QR code)))))))

--- myScript.js
+++ myScript.js
@@ -804,3 +804,12 @@ function checkCookie()
     }
 
 }
+
+function IFrame() {
+    var x = document.getElementById("QR");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
