e04aa9f74951da51c53d8cdf63d2841c7b4f165c
Vaximelirus <vivanov1@uclan.ac.uk>
2019-12-09 20:33:17 +0200

    Time Fix

--- JAVASCRIPT/myScript.js
+++ JAVASCRIPT/myScript.js
@@ -674,14 +674,14 @@ function LeaderBoard() {
 
 }
 function timeConverter(timestamp){
-    var a = new Date(timestamp);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
+    let a = new Date(timestamp);
+    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    let year = a.getFullYear();
+    let month = months[a.getMonth()];
+    let date = a.getDate();
+    let hour = a.getHours();
+    let min = a.getMinutes();
+    let sec = a.getSeconds();
     return date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
 }
 //location update
